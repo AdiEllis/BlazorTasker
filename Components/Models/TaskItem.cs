987fe4ddb7e46b1115e969003aeb187d770ba8b3
adi <adi.ellis3@gmail.com>
2026-02-11 11:54:15 +0200

    Start Tasker app

---
+++ Components/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTask.Components.Models
+{
+    public class TaskItem
+    {
+        public Guid Id {  get; set; }
+        [Required(ErrorMessage="Every task must have a name")]
+        public string Name { get; set; } = "";
+        public bool IsComplete { get; set; }
+
+    }
+}
